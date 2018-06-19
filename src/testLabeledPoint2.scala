04786a51ae879fcab49c486ae6f39916e25b4eb4
harold1994 <1093362865@qq.com>
2018-06-19 09:22:57 +0800

    添加推荐算法

--- src/testLabeledPoint2.scala
+++ src/testLabeledPoint2.scala
@@ -1,13 +1,12 @@
+
 import org.apache.spark.mllib.util.MLUtils
-import org.apache.spark._
+import org.apache.spark.{SparkConf, SparkContext}
 
 object testLabeledPoint2 {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf()
-      .setMaster("local")
-      .setAppName("testLabeledPoint2")
+    val conf = new SparkConf().setMaster("local").setAppName("testLabeledPoint2")
     val sc = new SparkContext(conf)
-    val mu = MLUtils.loadLibSVMFile(sc, "/media/harold/SpareDisk/SParkMLibDemo/src/labeleddata.txt")
+    val mu = MLUtils.loadLibSVMFile(sc,"file:///media/harold/SpareDisk/Java/SParkMLibDemo/labeleddata.txt")
     mu.foreach(println)
   }
 }
--- src/testVector.scala
+++ src/testVector.scala
@@ -2,9 +2,9 @@ import org.apache.spark.mllib.linalg.{Vector, Vectors}
 
 object testVector {
   def main(args: Array[String]): Unit = {
-    val vd : Vector = Vectors.dense('2','0','6' )
+    val vd : Vector = Vectors.dense(1,0,6)
     println(vd(2))
-    val vs: Vector = Vectors.sparse(4, Array(0,1,3,5), Array(9,5,2,7))
-    println(vs(2))
+    val vs: Vector = Vectors.sparse(6, Array(0,1,3,5), Array(9,5,2,7))
+    println(vs(5))
   }
 }
