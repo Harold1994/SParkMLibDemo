57194b46481a561116bf87607fcfa0fc47afecf2
harold1994 <1093362865@qq.com>
2018-06-27 20:52:14 +0800

    添加特征的提取，转换和选择

---
+++ dataframemllib/src/main/scala/StandardScalerExample.scala
@@ -0,0 +1,24 @@
+import org.apache.spark.ml.feature.StandardScaler
+import org.apache.spark.sql.SparkSession
+
+object StandardScalerExample {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("NormalizerExample")
+      .master("local")
+      .getOrCreate()
+
+    val dataFrame = spark.read.format("libsvm").load("data/mllib/sample_libsvm_data.txt")
+
+    val scaler = new StandardScaler()
+      .setInputCol("features")
+      .setOutputCol("scalerFeatures")
+      .setWithStd(true)
+      .setWithMean(false)
+
+    val scalerModel = scaler.fit(dataFrame)
+
+    val scaledData = scalerModel.transform(dataFrame)
+    scaledData.show()
+  }
+}
